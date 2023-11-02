c8a29b3b1d5c5f93f51928f74c8f91c11d5926bc
annafreidl <anna.freidl@syntax-institut.de>
2023-11-02 14:04:20 +0100

    solution

--- Aufgabe1/src/Aufgabe1.kt
+++ Aufgabe1/src/Aufgabe1.kt
@@ -1,3 +1,14 @@
 fun main() {
-    // Schreib hier deinen Code
+    print("Bitte gib eine ganze Zahl ein: ")
+    var zahl = readLine()?.toInt()
+
+    var summe = 0
+    var aktuelleZahl = 1
+
+    while (aktuelleZahl <= zahl) {
+        summe += aktuelleZahl
+        aktuelleZahl++
+    }
+    println("Die Summe aller natürlichen Zahlen von 1 bis $zahl ist: $summe")
 }
+
